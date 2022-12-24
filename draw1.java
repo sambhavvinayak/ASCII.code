da78e1ca71c8f201fa65ef06def3a32896b77094
Sam <58564925+sambhavvinayak@users.noreply.github.com>
2022-12-24 12:05:24 -0500

    Create draw1.java

---
+++ draw1.java
@@ -0,0 +1,27 @@
+
+
+public class draw1 {
+    public static void main(String[] args) {
+        
+        System.out.println("                    /\\-----------------------------\\         ");
+        System.out.println("                   //\\\\                             \\    ");
+        System.out.println("                  //  \\\\            ------           \\");
+        System.out.println("                 // || \\\\           |    |            \\");
+        System.out.println("                //  ||  \\\\          |    |             \\");
+        System.out.println("               // ====== \\\\         |____|              \\");
+        System.out.println("              //  ======  \\\\                             \\");
+        System.out.println("             //     ||     \\\\                             \\");
+        System.out.println("            //      ||      \\\\_____________________________\\");
+        System.out.println("            ------------------                              |");
+        System.out.println("            |                |                              |");
+        System.out.println("            |  ----          |                              |");
+        System.out.println("            | |    |         |                              |");
+        System.out.println("            |  ----          |                              |");
+        System.out.println("            |      ____      |                              |");
+        System.out.println("            |     |    |     |                              |");
+        System.out.println("            |     |   o|     |                              |");
+        System.out.println("            ------------------------------------------------|");
+
+
+    }
+}
